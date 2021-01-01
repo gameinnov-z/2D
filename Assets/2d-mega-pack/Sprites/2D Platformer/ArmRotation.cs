37747d7d8771b8203586d38f7e7083e39eaac43a
Priyanshu <sahupriyanshu108@gmail.com>
2021-01-01 12:34:08 +0530

    Arm Rotaion

---
+++ Assets/2d-mega-pack/Sprites/2D Platformer/ArmRotation.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using UnityEngine;               
+       
+public class ArmRotation : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition) - transform.position;
+        float angle = Mathf.Atan2(mousePos.y, mousePos.x) * Mathf.Rad2Deg;
+        Quaternion rot = Quaternion.AngleAxis(angle - 90, Vector3.forward);
+        transform.rotation = rot;
+    }
+}
